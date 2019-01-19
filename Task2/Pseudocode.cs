12847e4756d02c891ddd062cae7af0e418017b43
rishita.hariyani@gmail.com <rishita.hariyani@gmail.com>
2019-01-19 09:23:21 -0700

    Final Comit

---
+++ Task2/Pseudocode.cs
@@ -0,0 +1,40 @@
+
+//set up
+
+
+double Area=0.0;
+double Area2 = 0.0;
+
+//input
+
+"Enter value for rectangle1 height:" // lenght
+"Enter value for rectangle1 width:"
+"Enter value for rectangle2 height:" // lenght
+"Enter value for rectangle2 width:"
+
+
+//(Use Area to define 1 rectangle and Area2 for 2 rectangle)
+
+            //process
+          Area= h* w;// used this formua twice to calculate area for rectangle.
+
+//check conditions
+
+           if(Area > Area2)
+           put  Area + "is greater than" + Area2);
+           
+
+            else if (Area2 == Area)
+
+           put Area + "is equal to" + Area2
+
+            
+            else
+              put Area2 + "is greater than" + Area
+
+             
+   
+
+
+    
+   
--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -6,7 +6,60 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double Height1 = 0.0;
+            double Height2 = 0.0;
+            double Width1 = 0.0;
+            double Width2 = 0.0;
+            double Area = 0.0; 
+            double Area2 = 0.0;
+
+
+
+            Console.WriteLine("Please enter Height1 for first reactangle");
+            Height1 = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Please enter Width1 for first reactangle");
+            Width1 = double.Parse(Console.ReadLine());
+            
+            Area = Height1 * Width1;
+            Console.WriteLine("The Area is " + Area);
+            Console.ReadLine();
+            //While runing the task to continue please Press enter Key to continue the program
+
+            Console.WriteLine("Please enter Height2 for second reactangle");
+            Height2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter Width2 for Second reactangle");
+            Width2 = double.Parse(Console.ReadLine());
+
+            //While runing the task to continue please Press enter Key to continue the program
+            Area2 = Height2 * Width2;
+            Console.WriteLine("The Area2 is " + Area2);
+            Console.ReadLine();
+
+            //While runing the task to continue please Press enter Key to continue the program
+
+            if (Area > Area2)
+            {
+                Console.WriteLine(Area + "is greater than" + Area2);
+            }
+
+            else if (Area == Area2)
+
+            {
+
+                Console.WriteLine(Area + "is equal to" + Area2);
+
+            }
+            else
+
+            {
+                Console.WriteLine(Area2 + "is greater than" + Area);
+
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }
